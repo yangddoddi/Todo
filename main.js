dadc827cce96fa0ce75329bb334401ad451daeef
yangddoddi <pud1251@gmail.com>
2022-03-16 03:34:15 +0900

    retouch
    html 태그 의미 있게 변경, css 백그라운드 이미지 및 컨테이너 박스 쉐도우 변경, 자바스크립트 약간의 수정

--- main.js
+++ main.js
@@ -9,20 +9,18 @@ const clearAllBtn = document.querySelector(".todoList__foot__clearbtn");
 function addToDo(text) {
   const todoItem = document.createElement("div");
   const todoItemText = document.createElement("p");
-  const todoItemIcon = document.createElement("i");
+  const deleteBtn = document.createElement("i");
   todoItem.className = "todoList__main__item";
   todoItemText.className = "todoList__main__item--text";
-  todoItemIcon.className = "fa-solid fa-trash-can trash";
-  todoItem.append(todoItemText, todoItemIcon);
+  deleteBtn.className = "fa-solid fa-trash-can trash";
+  deleteBtn.addEventListener("click", () => {
+    todoList.removeChild(todoItem);
+    ChangeTaskNum();
+  });
+  todoItem.append(todoItemText, deleteBtn);
   todoList.append(todoItem);
   todoItemText.textContent = text;
-  let removeBtn = document.querySelectorAll(".trash");
-  removeBtn.forEach((e) => {
-    e.addEventListener("click", (event) => {
-      event.target.parentElement.remove();
-      ChangeTaskNum();
-    });
-  });
+  todoList.scrollTo(0, todoList.lastChild.getBoundingClientRect().y);
 }
 
 function ChangeTaskNum() {
@@ -38,23 +36,11 @@ function handleInput(e) {
   textInput.value = " ";
 }
 
-// `<div class="todoList__main__item">
-// <p class="todoList__main__item--text">히히</p><i class="fa-solid fa-trash-can trash"></i>
-// </div>`;
-
-// 인풋을 넣으면 html 요소를 추가해주는 이벤트 1(인풋)
-
-// trash 버튼을 누르면 해당 요소를 삭제
-
-// 요소의 수에 따라 할 일 체크
-
-// clearAll누르면 모든 자식 요소 삭제
-
 textInput.addEventListener("change", handleInput);
-//   document.querySelector(".todoList__main__item--text").innerHTML = text;
 
 clearAllBtn.addEventListener("click", () => {
   while (todoList.hasChildNodes()) {
     todoList.removeChild(todoList.firstChild);
   }
+  ChangeTaskNum();
 });
